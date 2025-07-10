bf05c1d94b8ec68332f47879d5af5bf97605ec90
Roman <roma.belozerov.90@mail.ru>
2025-07-10 14:09:04 +0300

    1

---
+++ TestGIT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void Jump()
+        {
+
+        }
+
+        public void Move()
+        {
+
+        }
+    }
+}
